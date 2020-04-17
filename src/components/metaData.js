d60e749bd846fef012d943f93402e00d141a72a5
Fernando XPS <ferna.zepeda@gmail.com>
2020-04-17 23:54:08 +0200

    add metadata

--- src/components/metaData.js
+++ src/components/metaData.js
@@ -2,9 +2,14 @@ import React from "react"
 import Helmet from "react-helmet"
 
 const MetaData = () => {
-    return(
-        <div>
-        <Helmet title="Fernando Zepeda"/>
+    return (
+        <div className="application">
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Fernando Zepeda</title>
+                <meta name="description" content="Data Scientist and Cloud Engineer. Curious and learning." />
+                <link rel="canonical" href="https://www.fernandozepeda.io" />
+            </Helmet>
         </div>
     )
 }
