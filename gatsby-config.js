e7c1e796dc2be3620bba224866455d74e9b55ef0
Fernando XPS <ferna.zepeda@gmail.com>
2020-04-19 01:17:33 +0200

    add google analytics tracking id env. variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,12 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
--- src/components/metaData.js
+++ src/components/metaData.js
@@ -8,8 +8,8 @@ const MetaData = () => {
                 <meta charSet="utf-8" />
                 <title>Fernando Zepeda</title>
                 <meta name="description" content="Data Scientist self-learning cloud engineering. Curious about AI and documenting the journey." />
-                <meta name="author" content="Fernando Zepeda">
-                <meta name="keywords" content="Data, Science, Engineering, Cloud, AI, Machine Learning">
+                <meta name="author" content="Fernando Zepeda" />
+                <meta name="keywords" content="Data, Science, Engineering, Cloud, AI, Machine Learning" />
                 <link rel="canonical" href="https://www.fernandozepeda.io" />
             </Helmet>
         </div>
