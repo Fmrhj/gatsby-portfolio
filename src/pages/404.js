727470d248eafebee65fc0ba7087fe2b00e655ba
Fernando XPS <ferna.zepeda@gmail.com>
2020-04-19 10:49:29 +0200

    add metadata to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,13 @@
 import React from "react"
 
 import '../../styles/styles.scss'
-
+import MetaData from "../components/metaData"
 
 const NotFoundPage = () => {
 
   return (
     <div className="wrapper">
+      <MetaData />
       <div className="container">
         <div className="not-found-block">
           <h1>404</h1>
