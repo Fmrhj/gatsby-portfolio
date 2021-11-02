2274a45d59d75b2ffde05b2a75521c2158bfbdd6
Fernando Zepeda <fernando.zepeda@xing.com>
2021-11-02 22:43:44 +0100

    change background

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 require(`katex/dist/katex.min.css`)
-require("prismjs/themes/prism-tomorrow.css")
+require("prismjs/themes/prism-okaidia.css")
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
 require("prismjs/plugins/command-line/prism-command-line.css")
