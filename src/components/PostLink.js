e9dd0fed9bb0450cb5e6734666c878aeba1d1407
Fernando Zepeda <fernando.zepeda@xing.com>
2022-02-12 01:01:44 +0100

    remove thumbnails

--- src/components/PostLink.js
+++ src/components/PostLink.js
@@ -5,9 +5,10 @@ import Thumbnail from "./Thumbnail"
 const PostLink = ({ post }) => (
   <div className="block-blog-posts-container">
     <Link to={post.frontmatter.path}>
+      {/*
       <div className="thumbnail">
         <Thumbnail src={post.frontmatter.thumbnail} />
-      </div>
+    </div>*/}
       <div className="content">
         <h3>{post.frontmatter.title}</h3>
         <small>{post.frontmatter.date}</small>
